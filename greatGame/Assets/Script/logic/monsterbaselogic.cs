ff402abbd96e05f5ca7f5d902fd89b76da825286
dsyaode <442977011@qq.com>
2015-02-08 16:41:24 +0800

    move

---
+++ greatGame/Assets/Script/logic/monsterbaselogic.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class monsterbaselogic : MonoBehaviour {
+
+	private float mFAcc = 2.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public Vector3 getMoveAcc(){
+		Vector3 v = new Vector3 ();
+		v.x = 0;
+		v.y = 0;
+		v.z = 0;
+		return v;
+	}
+
+	public Vector3 getFAcc(){
+		Vector3 v = new Vector3 ();
+		v.x = mFAcc;
+		v.y = mFAcc;
+		v.z = 0;
+		return v;
+	}
+}
