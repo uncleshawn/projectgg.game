96c9e0942a88c3891a7749888fd31b7eaec3cf0f
uncle_shawn <xiaoxiao@UNcledeMacBook-Pro.local>
2015-03-01 23:43:41 +0800

    3.1
    item_property 补上

---
+++ greatGame/Assets/Script/model/item_property.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class item_property : base_property {
+	
+	//public string typeName;
+	public List<itemType> iType ;
+	public itemType mType;
+	public string itemName;
+	public int itemId;
+	
+	// Use this for initialization
+	void Awake() {
+		this.gameObject.tag = "Item";
+	}
+	void Start () {
+		iType = new List<itemType> () ; 
+		
+		if(transform.gameObject.GetComponent<recover_Property>()){
+			Debug.Log("item has recover property!");
+			mType = itemType.recover;
+			iType.Add(mType);
+		}
+		if(transform.gameObject.GetComponent<enforce_Property>()){
+			Debug.Log("item has enforce property!");
+			mType = itemType.enforce;
+			iType.Add(mType);
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void selectItemType(string str){
+		switch(str){
+		default	: 		mType = itemType.none; 		break;
+		case "recover": mType = itemType.recover;	break;
+		case "enforce":	mType = itemType.enforce;  	break; 
+		}
+	}
+	
+	
+}
+
