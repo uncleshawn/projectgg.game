a0580e5ed36e2dae67cb9784f3d95a29b5064f75
uncle_shawn <xiaoxiao@UNcledeMacBook-Pro.local>
2015-03-06 22:59:25 +0800

    iconLogic

--- greatGame/Assets/Script/logic/iconlogic.cs
+++ greatGame/Assets/Script/logic/iconlogic.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class iconlogic : MonoBehaviour {
 
+	GameObject obj;
+	tk2dSprite icon;
 	// Use this for initialization
 	void Start () {
-	
+		icon = gameObject.GetComponent<tk2dSprite>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void  checkObjectType(){
+		obj = transform.parent.gameObject;
+		if(obj.GetComponent<item_property>()){
+			item_property itemproperty = obj.GetComponent<item_property>();
+			int iconId = itemproperty.ID;
+			if(iconId!=0){
+				icon.Collection = "";
+			}
+		}
 	}
 }
